1e00c403ea487e530d7b1b6937bd1b399ece3572
Emre Bülbül <87551092+Misleadinge@users.noreply.github.com>
2024-04-24 19:43:26 +0300

    homeloginsignup

--- roster-app/client/src/App.js
+++ roster-app/client/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import LogInPage from './LogIn';
+import LogInPage from './LogIn.js';
+import SignUpPage from './SignUp.js';
+import HomePage from './Home.js';
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<LogInPage />} />
+                <Route path="/" element={<HomePage/>} />
+                <Route path="/login" element={<LogInPage />} />
+                <Route path="/signup" element={<SignUpPage />} />
                 {/* Add more Route components here for additional pages */}
             </Routes>
         </BrowserRouter>
