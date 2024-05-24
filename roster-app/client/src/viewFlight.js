3c40e603fadbb27b8371c5e27e8dec4e91db9663
erayozturkk <erayozturk@sabanciuniv.edu>
2024-05-24 14:32:07 +0300

    Fix missing menu display in viewFlight.js

--- roster-app/client/src/viewFlight.js
+++ roster-app/client/src/viewFlight.js
@@ -55,7 +55,7 @@ const ViewFlight = () => {
                     <p><strong>Destination Airport Code:</strong> {flight.destination_airport_code}</p>
                     <p><strong>Distance:</strong> {flight.distance} KM</p>
                     <p><strong>Vehicle:</strong> {flight.vehicle_type}</p>
-                    <p><strong>Menu:</strong> {flightRoster.menu}</p>
+
                 </div>
             )}
             <div className='view-flight-container'>
@@ -68,6 +68,7 @@ const ViewFlight = () => {
                 {flight && currentView === 'plane' && flightRoster && (
                     console.log('Flight Roster:', flightRoster),
                     <>
+                        <p><strong>Menu:</strong> {flightRoster.menu}</p>
                         <SeatMap flightRoster={flightRoster} />
                     </>
                 )}
